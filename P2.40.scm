78598a4b388dfdf52279fb81a364475346a2b7a2
shell <shell@debian-home1>
2010-11-01 23:14:46 +0800

    解八皇后问题。

--- P2.40.scm
+++ P2.40.scm
@@ -32,4 +32,52 @@
 	 (not (= (caddr x) (car x)))
 	 (not (= (caddr x) (cadr x)))))
   (filter filter-tuple (map make-tuple (unique-pairs n))))
-(resolve 4 6)
+;; (resolve 4 6)
+(define (queens board-size)
+  (define (queen-cols i)
+    (if (= i 0)
+	(list (empty-board))
+	(filter
+	 (lambda (positions) (safe? positions))
+	 (flatmap
+	  (lambda (rest-of-queens)
+	    (map (lambda (j)
+		   (adjoin-position i j rest-of-queens))
+		 (enumerate-interval 1 board-size)))
+	  (queen-cols (- i 1))))
+	))
+  (queen-cols board-size)
+  )
+(define (empty-board) (list))
+(define (adjoin-position i j rest-of-queens)
+  (cons (cons i j) rest-of-queens))
+(define (safe? positions)
+  (define (safe-row? j positions)
+    (eq? '()
+	 (filter (lambda (y) (= y j))
+		 (map (lambda (p) (cdr p)) (cdr positions))
+		 )))
+  (define (safe-add? s positions)
+    (eq? '()
+	 (filter (lambda (e) (= e s))
+		 (map (lambda (p) (+ (car p) (cdr p))) (cdr positions))
+		 )))
+  (define (safe-dec? d positions)
+    (eq? '()
+	 (filter (lambda (e) (= e d))
+		 (map (lambda (p) (- (car p) (cdr p))) (cdr positions))
+		 )))
+  (and (safe-row? (cdar positions) positions)
+       (safe-add? (+ (caar positions) (cdar positions)) positions)
+       (safe-dec? (- (caar positions) (cdar positions)) positions))
+  )
+(queens 8)
+;; X O O O O O O O
+;; O O O O X O O O
+;; O O O O O O O X
+;; O O O O O X O O
+;; O O X O O O O O
+;; O O O O O O X O
+;; O X O O O O O O
+;; O O O X O O O O
+
